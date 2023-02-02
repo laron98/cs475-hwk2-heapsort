776be49ef9a67879ff39cab960538bdab9ac366f
laron98 <72889951+laron98@users.noreply.github.com>
2023-02-01 23:28:59 -0800

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,32 @@
 #include "employee.h"
 #include "heap.h"
 
+#define MAX_EMPLOYEES 5
+#define MAX_INPUT 25
+
 int main(int argc, char *argv[])
 {
 	//TODO
-	
+	struct Employee A[MAX_EMPLOYEES]; 
+	int c;
+	for(c = 0; c < MAX_EMPLOYEES; c++){
+
+		struct Employee e;
+		int in;
+		printf("Name: ");
+		scanf("%s", e.name);
+		printf("\n");
+
+		printf("Salary: ");
+		scanf("%d", &in);
+		e.salary = in;
+		printf("\n");
+
+		A[c] = e;
+	}
+
+
+	heapSort(A, MAX_EMPLOYEES);
+	printList(A, MAX_EMPLOYEES);
 	return 0;
 }
